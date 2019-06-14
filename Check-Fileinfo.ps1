f1f35ce145d54304094aba108d1bd28eaffc8233
TheGamer93 <50975764+TheGamer93@users.noreply.github.com>
2019-06-13 18:50:59 -0700

    Update Check-Fileinfo.ps1

--- Check-Fileinfo.ps1
+++ Check-Fileinfo.ps1
@@ -6,6 +6,12 @@ ForEach-Object {
     $Name = $_.Substring(0, $Lenpos).Trim()
     $len = $_.Substring($Lenpos, $LenLength).Trim()
     $Date = $_.Substring($Datepos).Trim()
-    Write-Host `
-    "Name='$Name' Len='$Len' Date='$Date'"
 }
+    $File = Get-Item $Name
+
+    If ($file.Length -ne $len)
+    { Write-Host "$Name Length has Changed"}
+    If ($File.LastWriteTime -ne $date)
+    { Write-host "$Name date has changed"}
+
+
